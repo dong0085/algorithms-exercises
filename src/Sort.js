59fbff1a1a53f36adea16969f50d65e2449e2dbc
eric dong <dong0085@algonquinlive.com>
2025-12-23 13:19:25 -0500

    uses sort.js to visualize bubble sort.

--- src/Sort.js
+++ src/Sort.js
@@ -1,12 +1,32 @@
-import React from "react";
-import { shuffle, range } from "lodash";
-import { App, snapshot, done, clear } from "./sort-visualizer";
+import React from 'react';
+import { shuffle, range } from 'lodash';
+import { App, snapshot, done, clear } from './sort-visualizer';
 
-import "./sort.css";
+import './sort.css';
+function bubbleSort(nums) {
+  // Loop through the array
+  const length = nums.length;
+  let iterations = 0;
+  let swapped;
+  do {
+    swapped = false;
+    snapshot(nums);
+    for (let i = 0; i < length - iterations; i++) {
+      // if current number is bigger than the next, do swap
+
+      if (nums[i] > nums[i + 1]) {
+        [nums[i], nums[i + 1]] = [nums[i + 1], nums[i]];
+        swapped = swapped ? swapped : true;
+      }
+    }
+    iterations++;
+  } while (swapped == true);
+  return nums;
+}
 
 function sort(array) {
   // do cool stuff here
-
+  bubbleSort(array);
   // call snapshot any time you do anything to the array
   // it's okay if you call it with duplicate value array,
   // it will deduplicate for you
@@ -15,7 +35,7 @@ function sort(array) {
 
 export default function SortComponent() {
   clear();
-  sort(shuffle(range(10)));
+  sort(shuffle(range(30)));
   done();
   return <App />;
 }
